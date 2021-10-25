6c96f19f9cad4e89bb09fed6b1e3065050d1ea40
João Gabriel Xavier <joaogabriel.informaticapgua@gmail.com>
2021-10-24 21:05:24 -0300

    commit inicial

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\DisciplinaController;
+use App\Http\Controllers\ProfessorController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('', function () {
+    return view('welcome');
+})->name('index');
+
+Route::resource('curso', CursoController::class);
+Route::resource('disciplina', DisciplinaController::class);
+Route::resource('professor', ProfessorController::class);
+Route::resource('aluno', AlunoController::class);
